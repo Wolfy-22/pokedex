fa7cbdb4c57352cfa96ab7e05c0c4cb6e8fa0d69
Wolfy-22 <bubblarr@gmail.com>
2025-11-14 01:37:44 -0500

    Did so much

--- repl.go
+++ repl.go
@@ -10,8 +10,6 @@ import (
 	"github.com/Wolfy-22/pokedex.git/internal/pokeapi"
 )
 
-var mapCount int
-
 type config struct {
 	pokeapiClient    pokeapi.Client
 	nextLocationsURL *string
@@ -21,11 +19,13 @@ type config struct {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func(*config, ...string) error
 }
 
+var Pokedex = make(map[string]pokeapi.Pokemon)
+
 func startRepl(cfg *config) {
-	mapCount = 0
+
 	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -37,10 +37,14 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := words[0]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -52,13 +56,13 @@ func startRepl(cfg *config) {
 	}
 }
 
-func commandExit(cfg *config) error {
+func commandExit(cfg *config, args ...string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, args ...string) error {
 	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
@@ -71,7 +75,17 @@ func commandHelp(cfg *config) error {
 	return nil
 }
 
-func commandMapf(cfg *config) error {
+func commandPokedex(cfg *config, args ...string) error {
+	fmt.Println()
+	fmt.Println("Your Pokedex:")
+	for _, pokemon := range Pokedex {
+		fmt.Printf(" - %s\n", pokemon.Name)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandMapf(cfg *config, args ...string) error {
 	fmt.Println()
 	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
@@ -88,10 +102,10 @@ func commandMapf(cfg *config) error {
 	return nil
 }
 
-func commandMapb(cfg *config) error {
+func commandMapb(cfg *config, args ...string) error {
 	fmt.Println()
 	if cfg.prevLocationsURL == nil {
-		return errors.New("you're on the first page")
+		return errors.New("You're on the first page\n")
 	}
 
 	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
@@ -105,6 +119,68 @@ func commandMapb(cfg *config) error {
 	for _, loc := range locationResp.Results {
 		fmt.Println(loc.Name)
 	}
+	fmt.Println()
+	return nil
+}
+
+func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a location name")
+	}
+
+	name := args[0]
+	location, err := cfg.pokeapiClient.GetLocation(name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("\nExploring %s...\n\n", location.Name)
+	fmt.Println("Found Pokemon: ")
+	for _, enc := range location.PokemonEncounters {
+		fmt.Printf(" - %s\n", enc.Pokemon.Name)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+	pokemon, err := cfg.pokeapiClient.GetPokemon(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("\nThrowing a Pokeball at %s...\n", pokemon.Name)
+	fmt.Printf("%s was caught!\n\n", pokemon.Name)
+	Pokedex[name] = pokemon
+
+	return nil
+
+}
+
+func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+	pokemon := Pokedex[name]
+
+	fmt.Printf("\nName: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Printf("Base Stats:\n")
+	for i := range 6 {
+		fmt.Printf("  -%s: %d\n", pokemon.Stats[i].Stat.Name, pokemon.Stats[i].BaseStat)
+	}
+	fmt.Printf("Types:\n")
+	for i := range len(pokemon.Types) {
+		fmt.Printf("  - %s\n", pokemon.Types[i].Type.Name)
+	}
+
 	fmt.Println()
 	return nil
 }
@@ -123,6 +199,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "Explore a location",
+			callback:    commandExplore,
+		},
 		"map": {
 			name:        "map",
 			description: "Get the next page of locations",
@@ -137,6 +218,21 @@ func getCommands() map[string]cliCommand {
 			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
+		},
+		"catch": {
+			name:        "catch <pokemon_name>",
+			description: "Adds a Pokemon to your Pokedex",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "Checks a Pokemon's stats",
+			callback:    commandInspect,
+		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "Lists all caught Pokemon",
+			callback:    commandPokedex,
 		},
 	}
 }
